fda9fdd69e4a6426b4e3af29cda877bcb30b5afc
michaelrauh <michaelrauh@ymail.com>
2023-03-11 11:17:11 -0500

    build tree from corpus

--- src/lib.rs
+++ src/lib.rs
@@ -35,13 +35,59 @@ impl Tree {
     fn step_down(&self, name: String) -> Option<&Self> {
         self.children.get(&name)
     }
+
+    fn split_sentence(sentence: String) -> Vec<String> {
+        sentence
+            .split_ascii_whitespace()
+            .map(|x| x.to_string())
+            .collect()
+    }
+
+    pub fn from_corpus(corpus: String) -> Self {
+        let mut tree = Tree::default();
+        let sentences = split_corpus(corpus);
+        for sentence in sentences {
+            for suffix in suffixes(Self::split_sentence(sentence)) {
+                tree.add_phrase(suffix)
+            }
+        }
+        tree
+    }
+}
+
+fn suffixes(xs: Vec<String>) -> Vec<Vec<String>> {
+    let mut acc = vec![];
+    for i in 0..xs.len() {
+        let sliced: Vec<String> = xs[i..].to_vec();
+        acc.push(sliced);
+    }
+    acc
+}
+
+fn split_corpus(x: String) -> Vec<String> {
+    x.split_terminator(&['.', '!', '?', ';'])
+        .filter(|x| !x.is_empty())
+        .map(|x| x.trim())
+        .map(|sentence| {
+            sentence
+                .split_ascii_whitespace()
+                .map(|s| {
+                    s.chars()
+                        .filter(|c| c.is_alphabetic())
+                        .collect::<String>()
+                        .to_lowercase()
+                })
+                .collect::<Vec<_>>()
+                .join(" ")
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use std::vec;
 
-    use crate::Tree;
+    use crate::{split_corpus, suffixes, Tree};
 
     #[test]
     fn it_defaults() {
@@ -107,5 +153,72 @@ mod tests {
         assert_eq!(a_tree.children_names().len(), 2);
         assert!(a_tree.children_names().contains(&"b".to_string()));
         assert!(a_tree.children_names().contains(&"c".to_string()));
+    }
+
+    #[test]
+    fn it_calculates_suffixes() {
+        assert_eq!(
+            suffixes(vec![
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+                "d".to_string()
+            ]),
+            vec![
+                vec![
+                    "a".to_string(),
+                    "b".to_string(),
+                    "c".to_string(),
+                    "d".to_string()
+                ],
+                vec!["b".to_string(), "c".to_string(), "d".to_string()],
+                vec!["c".to_string(), "d".to_string()],
+                vec!["d".to_string()]
+            ]
+        )
+    }
+
+    #[test]
+    fn it_splits_a_corpus_to_sentences() {
+        assert_eq!(
+            split_corpus("a b! c d. e, f? g: h;".to_string()),
+            vec![
+                "a b".to_string(),
+                "c d".to_string(),
+                "e f".to_string(),
+                "g h".to_string()
+            ]
+        );
+    }
+
+    #[test]
+    fn it_ingests_a_corpus() {
+        let t = Tree::from_corpus("a b! c d. a, c? b: d;".to_string());
+
+        //root has four children
+        assert_eq!(t.children_names().len(), 4);
+        assert!(t.children_names().contains(&"a".to_string()));
+        assert!(t.children_names().contains(&"b".to_string()));
+        assert!(t.children_names().contains(&"c".to_string()));
+        assert!(t.children_names().contains(&"d".to_string()));
+
+        // a has two children
+        assert_eq!(t.step_down("a".to_string()).unwrap().children_names().len(), 2);
+        assert!(t.step_down("a".to_string()).unwrap().children_names().contains(&"b".to_string()));
+        assert!(t.step_down("a".to_string()).unwrap().children_names().contains(&"c".to_string()));
+
+        // b has one child
+        assert_eq!(t.step_down("b".to_string()).unwrap().children_names().len(), 1);
+        assert!(t.step_down("b".to_string()).unwrap().children_names().contains(&"d".to_string()));
+
+        // c has one child
+        assert_eq!(t.step_down("c".to_string()).unwrap().children_names().len(), 1);
+        assert!(t.step_down("c".to_string()).unwrap().children_names().contains(&"d".to_string()));
+
+        // d has no children
+        assert_eq!(t.step_down("d".to_string()).unwrap().children_names().len(), 0);
+
+        // bad paths give back none
+        assert!(t.step_down("a".to_string()).unwrap().step_down("a".to_string()).is_none());
     }
 }
